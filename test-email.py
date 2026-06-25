8a36ffd4718e72b51e154e98f4301d8f8471daad
Himanshu Daga <40109922+dagahimanshu@users.noreply.github.com>
2026-06-25 19:58:14 +0530

    Personal SMTP key exposed

--- test-email.py
+++ test-email.py
@@ -3,7 +3,7 @@ import requests
 def send_simple_message():
   	return requests.post(
   		"https://api.mailgun.net/v3/sandbox14a84927cb824d1eaf788e8bc4b47baa.mailgun.org/messages",
-  		auth=("api", os.getenv('API_KEY', '9cb17d52fa5f845f569e2047a56fbdbc-d638fab7-191439d3')),
+  		auth=("api", os.getenv('API_KEY', '')),
   		data={"from": "Mailgun Sandbox <postmaster@sandbox14a84927cb824d1eaf788e8bc4b47baa.mailgun.org>",
 			"to": "Himanshu <himanshu-navan@outlook.com>",
   			"subject": "Hello Himanshu",
